a7cb1efb86431fb67193bb817421fe0b7257c6ce
fer7G <fernand07gn@gmail.com>
2023-05-20 20:50:57 +0200

    batch update

--- KnowledgeBasedSystems/Project/run.clp.bat
+++ KnowledgeBasedSystems/Project/run.clp.bat
@@ -3,4 +3,8 @@
 (defglobal ?*debug-print* = nil) ;nil vs t
 (load "Classes/ontologia.clp")
 (reset)
-(load "Instances/ingredients.clp")
+(load-instances "Instances/ingredients.pins")
+(load-instances "Instances/nutritional-value.pins")
+(load-instances "Instances/minerals.pins")
+(load-instances "Instances/vitamins.pins")
+(run)
