dccb3aad4dabbb232587fdcf922b788fcf4ffcd4
go3212 <go3212@hotmail.com>
2023-05-24 00:42:10 +0200

    Fix

--- KnowledgeBasedSystems/Project/run.clp.bat
+++ KnowledgeBasedSystems/Project/run.clp.bat
@@ -3,8 +3,7 @@
 (defglobal ?*debug-print* = nil) ;nil vs t
 (load "Classes/ontologia.clp")
 (reset)
-(load-instances "Instances/ingredients.pins")
-(load-instances "Instances/nutritional-value.pins")
 (load-instances "Instances/minerals.pins")
-(load-instances "Instances/vitamins.pins")
+(load-instances "Instances/nutritional-value.pins")
+(load-instances "Instances/ingredients.pins")
 (run)
